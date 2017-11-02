ca4a5e0764ea98161e38cd9b39db4dd785c3fa7f
Oliver Braun <ob@cs.hm.edu>
2017-11-02 09:55:30 +0100

    Aktueller Stand nach Vorlesung 2.11.

--- helloWorld/HelloWorld.java
+++ helloWorld/HelloWorld.java
@@ -1,5 +1,19 @@
+/* Hier wird Hallo Welt ausgegeben. */
 class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hallo Olli!");
+    int noOfCoffeeForToday;
+
+    noOfCoffeeForToday = 5;
+    // hier ist i 5
+
+    System.out.println("Montag: " + noOfCoffeeForToday + " Kaffee");
+
+    noOfCoffeeForToday = 7;    // ab hier ist i 7
+
+    System.out.println("Dienstag: " + noOfCoffeeForToday + " Kaffee");
+
+    System.out.println("Mittwoch: " + (noOfCoffeeForToday + noOfCoffeeForToday + 1) + " Kaffee");
+
+
   }
 }
