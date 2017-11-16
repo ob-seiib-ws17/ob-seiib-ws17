eb0789f691d3a6f71646312f4f7df0e6043a4d2b
Oliver Braun <ob@cs.hm.edu>
2017-11-16 10:42:55 +0100

    aktueller Stand nach Vorlesung 16.11.

--- gradle-project/src/main/java/App.java
+++ gradle-project/src/main/java/App.java
@@ -1,5 +1,22 @@
 public class App {
   public static void main(String[] args) {
-    
+    // Argumente einlesen und umwandlen
+    final int a =  Integer.parseInt(args[0]);
+    final int b =  Integer.parseInt(args[1]);
+    final int c =  Integer.parseInt(args[2]);
+
+    // eigentliche Berechnung
+    int max = a;
+
+    if (b > max) {
+      max = b;
+      System.out.printf("Setze b");
+    }
+
+    if (c > max)
+      max = c;
+
+    // Ausgabe
+    System.out.printf("max = %d%n", max);
   }
 }
