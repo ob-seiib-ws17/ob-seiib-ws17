46710d69e53a6bd55a7b60446f43f9ef59e1b42e
Oliver Braun <ob@cs.hm.edu>
2017-11-30 11:04:39 +0100

    englische Variablennamen!

--- gradle-project/src/main/java/MultiplicationTable.java
+++ gradle-project/src/main/java/MultiplicationTable.java
@@ -1,16 +1,16 @@
 class MultiplicationTable {
   public static void main(String[] args) {
     int maximum = Integer.parseInt(args[0]);
-    int zeile = 1;
-    int spalte;
-    while (zeile <= maximum) { // äußere Schleife
-      spalte = 1;
-      while (spalte <= maximum) { // innere Schleife
+    int row = 1;
+    int column;
+    while (row <= maximum) { // äußere Schleife
+      column = 1;
+      while (column <= maximum) { // innere Schleife
         // System.out.printf("%4d", zeile * spalte);
         // spalte = spalte + 1;
-        System.out.println("Spalte = " + spalte++);
+        System.out.println("Spalte = " + column++);
       }
-      ++zeile;
+      ++row;
       System.out.println();
     }
   }
