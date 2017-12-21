788edc845e2a42d33c43917406c5cf8534b6ac6c
Oliver Braun <ob@cs.hm.edu>
2017-12-21 10:01:30 +0100

    Aktueller Stand nach der heutigen Vorlesug

--- gradle-project/src/main/java/App.java
+++ gradle-project/src/main/java/App.java
@@ -1,20 +1,45 @@
 class App {
   public static void main(String[] args) {
 
-    int i = 5;
+    Rational a = new Rational();
+    a.numer = 1;
+    a.denom = 9;
 
-    Rational r = null;
+    Rational b = a;
 
-    r = new Rational();
+    Rational c = new Rational();
+    c.numer = 1;
+    c.denom = 9;
 
-    i = 7;
+    if (a == b) {
+      System.out.println("a ist gleich b");
+    } else {
+      System.out.println("a ist verschieden von b");
+    }
 
-    r.numer = 5;
-    r.denom = 3;
+    if (a.numer == c.numer && a.denom == c.denom) {
+      System.out.println("a ist gleich c");
+    } else {
+      System.out.println("a ist verschieden von c");
+    }
 
-    i = i + 2;
+    int i = 7;
+    int j = 7;
 
-    i = r.numer + 5 * r.denom;
+    if (i == j) {
+      System.out.println("i ist gleich j");
+    } else {
+      System.out.println("i ist verschieden von j");
+    }
+    String help1 = "help";
+    String help2 = "h";
+    help2 = help2 + "elp";
 
+    if (help1.equals(help2)) {
+      System.out.println(help1 + " ist gleich " + help2);
+    } else {
+      System.out.println(help1 + " ist verschieden von " + help2);
+
+    }
   }
 }
