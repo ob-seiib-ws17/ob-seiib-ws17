1dabc44598ad0d7503fc2d05ad2f44567d808fac
Oliver Braun <ob@cs.hm.edu>
2018-01-10 19:11:34 +0100

    weiteres Projekt für live coding

---
+++ gradle-intellij-project/src/main/java/App.java
@@ -0,0 +1,10 @@
+class App {
+  public static void main(String[] args) {
+
+    Rational a = new Rational();
+    a.numer = 1;
+    a.denom = 9;
+
+
+  }
+}
---
+++ gradle-intellij-project/src/main/java/Rational.java
@@ -0,0 +1,4 @@
+class Rational {
+  int numer;
+  int denom;
+}
