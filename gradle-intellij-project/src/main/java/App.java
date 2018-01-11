598f8b7d19d70b541156655a1002c74e081eca74
Oliver Braun <ob@cs.hm.edu>
2018-01-11 10:33:56 +0100

    Stand Vorlesung 11.01.18

--- gradle-intellij-project/src/main/java/App.java
+++ gradle-intellij-project/src/main/java/App.java
@@ -6,5 +6,25 @@ class App {
     a.denom = 9;
 
 
+    a.print();
+
+    a.extend(2 * a.numer / (int) Math.sqrt(6)) ;
+
+    a.extend(3);
+
+    a.extend(123);
+
+    a.extend(5);
+
+    // System.out.printf("%d/%d%n", a.numer, a.denom);
+
+//    Rational b = new Rational();
+//    b.numer = 1;
+//    b.denom = 90;
+//
+//    b.print();
+
+    // System.out.printf("%d/%d%n", b.numer, a.denom);
+
   }
 }
--- gradle-intellij-project/src/main/java/Rational.java
+++ gradle-intellij-project/src/main/java/Rational.java
@@ -1,4 +1,16 @@
 class Rational {
   int numer;
   int denom;
+
+
+  void print() {
+    System.out.printf("%d/%d%n", this.numer, this.denom);
+  }
+
+  void extend(final int x) {
+    x = 3;
+    numer *= x;
+    denom *= x;
+  }
+
 }
